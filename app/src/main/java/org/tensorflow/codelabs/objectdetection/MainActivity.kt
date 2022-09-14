516ef06f0abdfd995043181c92342dec39aac1d7
OhJiwoo <74577768+Ohjiwoo-lab@users.noreply.github.com>
2022-09-14 21:48:23 +0900

    final version

--- app/src/main/java/org/tensorflow/codelabs/objectdetection/MainActivity.kt
+++ app/src/main/java/org/tensorflow/codelabs/objectdetection/MainActivity.kt
@@ -16,27 +16,36 @@
 
 package org.tensorflow.codelabs.objectdetection
 
+import android.Manifest
 import android.app.Activity
 import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.*
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import androidx.exifinterface.media.ExifInterface
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.tensorflow.lite.support.image.TensorImage
-import org.tensorflow.lite.task.vision.detector.Detection
 import org.tensorflow.lite.task.vision.detector.ObjectDetector
 import java.io.File
 import java.io.IOException
@@ -45,7 +54,7 @@ import java.util.*
 import kotlin.math.max
 import kotlin.math.min
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener{
     companion object {
         const val TAG = "TFLite - ODT"
         const val REQUEST_IMAGE_CAPTURE: Int = 1
@@ -59,8 +68,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var imgSampleThree: ImageView
     private lateinit var tvPlaceholder: TextView
     private lateinit var currentPhotoPath: String
+    private lateinit var tts: TextToSpeech
+    var SttIntent: Intent? = null
+    private lateinit var mRecognizer: SpeechRecognizer
+    private lateinit var cThis: Context
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        cThis = this
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -75,6 +89,40 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         imgSampleOne.setOnClickListener(this)
         imgSampleTwo.setOnClickListener(this)
         imgSampleThree.setOnClickListener(this)
+
+        // 음성 출력 생성, 리스너 초기화
+        tts = TextToSpeech(cThis) { status ->
+            if (status != TextToSpeech.ERROR) {
+                tts.language = Locale.KOREAN
+            }
+        }
+
+        // 음성 인식
+        SttIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        SttIntent!!.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, applicationContext.packageName)
+        SttIntent!!.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR") // 한국어 사용
+
+        if (ContextCompat.checkSelfPermission(
+                cThis,
+                Manifest.permission.RECORD_AUDIO
+            ) !== PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this@MainActivity,
+                arrayOf<String>(Manifest.permission.RECORD_AUDIO),
+                1
+            )
+            //권한을 허용하지 않는 경우
+        } else {
+            //권한을 허용한 경우
+            try {
+                mRecognizer = SpeechRecognizer.createSpeechRecognizer(cThis)
+                mRecognizer.setRecognitionListener(listener)
+                mRecognizer.startListening(SttIntent)
+            } catch (e: SecurityException) {
+                e.printStackTrace()
+            }
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -305,7 +353,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             val box = it.boundingBox
             canvas.drawRect(box, pen)
 
-
             val tagSize = Rect(0, 0, 0, 0)
 
             // calculate the right font size
@@ -326,8 +373,74 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 it.text, box.left + margin,
                 box.top + tagSize.height().times(1F), pen
             )
+            System.out.println(it.text)
+            val text = it.text.split(",")[0]
+            FuncVoiceOut("이것은 " + text + "입니다.")
         }
         return outputBitmap
+    }
+
+    //음성 메세지 출력용
+    private fun FuncVoiceOut(OutMsg: String) {
+        if (OutMsg.length < 1) return
+        tts.setPitch(1.0f) //목소리 톤1.0
+        tts.setSpeechRate(1.0f) //목소리 속도
+        tts.speak(OutMsg, TextToSpeech.QUEUE_FLUSH, null)
+
+        //어플이 종료할때는 완전히 제거
+    }
+
+    // RecognizerIntent 객체에 할당할 listener 생성
+    private val listener: RecognitionListener = object : RecognitionListener {
+        override fun onReadyForSpeech(params: Bundle) {
+            Toast.makeText(applicationContext, "음성인식을 시작합니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onBeginningOfSpeech() {}
+        override fun onRmsChanged(rmsdB: Float) {}
+        override fun onBufferReceived(buffer: ByteArray) {}
+        override fun onEndOfSpeech() {}
+        override fun onError(error: Int) {
+            val message: String
+            message = when (error) {
+                SpeechRecognizer.ERROR_AUDIO -> "오디오 에러"
+                SpeechRecognizer.ERROR_CLIENT -> "클라이언트 에러"
+                SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS -> "퍼미션 없음"
+                SpeechRecognizer.ERROR_NETWORK -> "네트워크 에러"
+                SpeechRecognizer.ERROR_NETWORK_TIMEOUT -> "네트웍 타임아웃"
+                SpeechRecognizer.ERROR_NO_MATCH -> "찾을 수 없음"
+                SpeechRecognizer.ERROR_RECOGNIZER_BUSY -> "RECOGNIZER가 바쁨"
+                SpeechRecognizer.ERROR_SERVER -> "서버가 이상함"
+                SpeechRecognizer.ERROR_SPEECH_TIMEOUT -> "말하는 시간초과"
+                else -> "알 수 없는 오류임"
+            }
+            Toast.makeText(applicationContext, "에러가 발생하였습니다. : $message", Toast.LENGTH_SHORT).show()
+            mRecognizer.startListening(SttIntent)
+        }
+
+        override fun onResults(results: Bundle) {
+            // 말을 하면 ArrayList에 단어를 넣고 textView에 단어를 이어줍니다.
+            val matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+            for (i in matches!!.indices) {
+                if (matches[i].contains("시작")) {
+                    captureImageFab.performClick()
+                } else {
+                    mRecognizer.startListening(SttIntent)
+                }
+            }
+        }
+
+        override fun onPartialResults(partialResults: Bundle) {}
+        override fun onEvent(eventType: Int, params: Bundle) {}
+    }
+
+    //카톡으로 이동을 했는데 음성인식 어플이 종료되지 않아 계속 실행되는 경우를 막기위해 어플 종료 함수
+    override fun onDestroy() {
+        super.onDestroy()
+        tts.stop()
+        tts.shutdown()
+        mRecognizer.destroy()
+        mRecognizer.cancel()
     }
 }
 
